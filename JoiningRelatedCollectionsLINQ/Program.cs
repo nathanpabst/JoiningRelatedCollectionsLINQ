f2a841d0ac274915a5a8bb60d0e643ddf344e1af
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-21 17:04:34 -0500

    Add project files.

---
+++ JoiningRelatedCollectionsLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JoiningRelatedCollectionsLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
